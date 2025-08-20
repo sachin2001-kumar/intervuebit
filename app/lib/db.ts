ad5e78cd64b52d7fe2d23c4bf02cae028966db4b
Sachin Kumar Dagar <sachinkumardagar20001@gmail.com>
2025-08-20 23:47:34 +0530

    add updates

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,5 +1,8 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "@/app/drizzle/Schema";
 
-const sql = neon(process.env.DATABASE_URL!);
-const db = drizzle({ client: sql });
+const sql = neon(
+  "postgresql://neondb_owner:npg_RASeq0cz7COv@ep-gentle-feather-a2mhsay4-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+);
+export const db = drizzle(sql, { schema });
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
+import { Footer } from "./Components/Footer";
+
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-purple-500 via-blue-400 to-yellow-200"></div>
+    <div className="min-h-screen bg-gradient-to-r from-purple-500 via-blue-400 to-yellow-200">
+      <Footer />
+    </div>
   );
 }
