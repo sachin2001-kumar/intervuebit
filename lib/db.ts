a1670044dbf820cd13e3a66a9513648d904b5711
Sachin Kumar Dagar <sachinkumardagar20001@gmail.com>
2025-08-23 01:31:09 +0530

    Add Updates

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,7 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
-import * as schema from "@/app/drizzle/Schema";
+import * as schema from "@/drizzle/Schema";
 
-const sql = neon(
-  "postgresql://neondb_owner:npg_RASeq0cz7COv@ep-gentle-feather-a2mhsay4-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
-);
+const sql = neon(process.env.NEON_DATABASE_URL!);
+
 export const db = drizzle(sql, { schema });
