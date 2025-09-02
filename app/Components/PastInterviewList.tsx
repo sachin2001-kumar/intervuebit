72a95c00aa3f5d4837ba0f4bd7be126f8224124f
Sachin Kumar Dagar <sachinkumardagar20001@gmail.com>
2025-09-02 21:50:58 +0530

    Add Past Interview

--- app/Components/PastInterviewList.tsx
+++ app/Components/PastInterviewList.tsx
@@ -4,6 +4,9 @@ import { useUser } from "@clerk/nextjs";
 import { db } from "@/lib/db";
 import { InterviewDetails } from "@/drizzle/Schema";
 import { desc, eq, InferSelectModel } from "drizzle-orm";
+import { useSectionInView } from "@/lib/hooks";
+import { motion } from "framer-motion";
+import { Button } from "@/ui/button";
 
 type InterviewDetailsType = InferSelectModel<typeof InterviewDetails>;
 
@@ -27,19 +30,52 @@ export const PastInterviewList = () => {
   //   SetInterviewList(data);
   // };
 
+  const { ref } = useSectionInView("Past Interview");
+
   return (
-    <div className="flex max-w-6xl mx-auto h-[45rem] bg-green-300 min-h-screen">
-      {/* {InterviewList.map((interview) => (
+    <section
+      id="Past-Interview"
+      ref={ref}
+      className="flex flex-col items-center p-6 border-2 rounded-4xl border-gray-800 max-w-6xl mx-auto bg-[#5b4d57]  scroll-mt-24 shadow-lg hover:shadow-2xl hover:shadow-[#b4a5b4] transition-all"
+    >
+      <motion.h2
+        initial={{ opacity: 0, y: -30 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.6, ease: "easeOut" }}
+        viewport={{ amount: 0.3, once: true }}
+        className="flex text-4xl text-center font-extrabold md:text-5xl text-gray-900"
+      >
+        {" "}
+        Past Interviews{" "}
+      </motion.h2>
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.4, duration: 0.8 }}
+        className=" mt-12 gap-10 flex flex-col items-center p-6"
+      >
+        <motion.p
+          initial={{ opacity: 0, y: -30 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          className="text-3xl font-medium text-gray-900 text-center"
+        >
+          You're required login to get Access to your Past Interviews.{" "}
+        </motion.p>
+        <Button className="flex bg-cyan-700 text-gray-900 hover:bg-cyan-600 font-bold w-fit text-3xl p-6 rounded-2xl">
+          Get Started
+        </Button>
+      </motion.div>
+    </section>
+  );
+};
+
+{
+  /* {InterviewList.map((interview) => (
         <div key={interview.id} className="p-2 border-b">
           <p>
             <strong>{interview.JobPosition}</strong> {interview.JobExp} yrs
           </p>
           <p className="text-sm text-gray-500">{interview.JobDesc}</p>
         </div>
-      ))} */}
-      <h1 className="flex text-black font-extrabold text-6xl items-center justify-center">
-        Past Interview ......
-      </h1>
-    </div>
-  );
-};
+      ))} */
+}
